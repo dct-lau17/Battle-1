30bbe04c5f3af17a0dd4b6a3add7d9a43b1d3dbc
Daniel Lau <daniellau@Daniels-MacBook-Pro-2.local>
2018-06-29 14:34:21 +0100

    ability to switch turns after an attack

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -4,7 +4,6 @@ feature "attack" do
   scenario "player 1 attacks player 2" do
     sign_in_and_play
     click_button('Attack')
-
     expect(page).to have_content 'Jay attacked Daniel!'
   end
 
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,8 +2,8 @@ require 'game'
 
 describe Game do
   subject(:game) { described_class.new(player_1, player_2) }
-  let(:player_1) { double (:player) }
-  let(:player_2) { double (:player) }
+  let(:player_1) { double(:player) }
+  let(:player_2) { double(:player) }
 
   describe "#default" do
     it "returns player 1" do
@@ -18,6 +18,19 @@ describe Game do
     it "damage the player" do
       expect(player_2).to receive(:receive_damage)
       game.attack(player_2)
+    end
+  end
+
+  describe '#current_turn' do
+    it 'starts as player 1' do
+      expect(game.current_turn).to eq player_1
+    end
+  end
+
+  describe '#switch_turns' do
+    it 'switches the turn' do
+      game.switch_turns
+      expect(game.current_turn).to eq player_2
     end
   end
 end
